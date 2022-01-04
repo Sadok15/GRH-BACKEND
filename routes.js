c892c9ea0d1cbc562a5fa598b5bf05e8003f30fe
Mattoussi Nour <nour.mattoussi@neopolis-dev.com>
2022-01-04 21:25:51 +0100

    add route.js

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const candidate_controller = require("./controllers/candidateController")
-
+const opportunity_controller=require("./controllers/opportunityController")
 
 
 router.post("/get_cand", candidate_controller.get_candidate)
@@ -10,5 +10,5 @@ router.post("/get_cand", candidate_controller.get_candidate)
 
 router.post("/add_cv", candidate_controller.add_cv_candidate)
 
-
+router.get("list_opportunity",opportunity_controller.list_opportunity)
 module.exports = router
