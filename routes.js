26a7946e1e9bc8e5274865f64e796b68867265fc
Mattoussi Nour <nour.mattoussi@neopolis-dev.com>
2022-01-04 23:12:30 +0100

    add models

--- routes.js
+++ routes.js
@@ -3,14 +3,18 @@ const router = express.Router()
 
 const candidate_controller = require("./controllers/candidateController")
 const opportunity_controller=require("./controllers/opportunityController")
-
+const societe_controller = require("./controllers/societeController")
+const contrat_controller = require("./controllers/contratController")
+const status_controller = require ("./controllers/statusController")
 
 router.post("/get_cand", candidate_controller.get_candidate)
 
 
 router.post("/add_cv", candidate_controller.add_cv_candidate)
 
-router.get("list_opportunity",opportunity_controller.list_opportunity)
-router.post("add_opportunity",opportunity_controller.add_opportunity)
-
+router.get("/list_opportunity",opportunity_controller.list_opportunity)
+router.post("/add_opportunity",opportunity_controller.add_opportunity)
+router.post("/add_societe",societe_controller.add_societe)
+router.post("/add_status",status_controller.add_status)
+router.post("/add_contrat",contrat_controller.add_contrat)
 module.exports = router
