ea4a5db73c41fa85cd225c207d939b01cc5471dd
sadok <sadokfrouja1im8@gmail.com>
2022-01-04 16:09:05 +0100

    add api save candidate

--- models/competence.js
+++ models/competence.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose")
 
-const comp = mongoose.Schema({
-    id_comp : {
-        type: Number,
-        required: true,
-    },
+const comp_schema = mongoose.Schema({
+
     id_cv : {
-        type: mongoose.Schema.Types.ObjectId, ref: 'CV'
+        type: mongoose.Schema.Types.ObjectId,
+         ref: 'CV'
     },
     detail:{
         type:String,
     }
 })
 
-// module.exports = mongoose.model("Competence", comp)
+module.exports = mongoose.model("Competence", comp_schema)
--- routes.js
+++ routes.js
@@ -8,9 +8,7 @@ const candidate_controller = require("./controllers/candidateController")
 router.post("/get_cand", candidate_controller.get_candidate)
 
 
-router.post("/add_cv", 
-    candidate_controller.add_cv_candidate
-)
+router.post("/add_cv", candidate_controller.add_cv_candidate)
 
 
 module.exports = router
