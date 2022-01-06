3c6ef403fa39dab2102c224dfd1dc07c595c880b
sadok <sadokfrouja1im8@gmail.com>
2022-01-06 10:30:31 +0100

    save add postuler

--- controllers/opportunityController.js
+++ controllers/opportunityController.js
@@ -44,7 +44,7 @@ exports.postuler = async function(req, res){
     
     const opp_cond = new OpportunityCond()
     const opportunity = await Opportunity.findOne({ _id:req.body.opportunity.id_opportunity})
-    const condidat = await Candidate.findOne({ _id:req.body.opportunity.id_condidat})
+    const condidat = await Candidate.findOne({ mail:req.body.mail, mdp:req.body.mdp})
     opp_cond.id_condidat = condidat._id 
     opp_cond.id_opportunity = opportunity._id 
     await opp_cond.save()
