f5d3f9b73c006637a577d7298d5a6aded80a4748
Sadok Frouja <sadok.frouja@defensy.io>
2022-12-03 20:00:07 +0100

    refactor api add user

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -32,52 +32,63 @@ exports.get_candidate =  async function(req,res){
 
 
 exports.add_cv_candidate = async function(req, res){
+
+    try {
+        // add candidate
+        dict = req.body
+        const candidate = new Candidate({
+            mail: dict.email,
+            mdp: dict.mdp,
+            num_tel: dict.num_tel,
+        }) 
+
+        
+        await candidate.save()
+
+        // add cv 
+            const cv_cand = new cv()
+            cv_cand.id_cand = candidate._id
+            cv_cand.github = dict.github
+        
+        await cv_cand.save()
+
+        
+        // add competences
+        for(var k=0; k< dict["competences"].length; k++ ){
+            const comp = new competence()
+            comp.id_cv = cv_cand._id
+            comp.detail= dict.competences[k]
+            await comp.save()
+        }
+
+
+        // add experience
+        for(var j=0; j< dict["experiences"].length ; j++ ){
+
+            exp_dict = dict.experiences[j]
+            const exp = new experience()
+        
+            exp.id_cv = cv_cand._id
+            exp.duree = exp_dict["duree"]
+            exp.detail= exp_dict["detail"]
+            await exp.save()
+        }
+        
+        // send result
+        return res.json({ 
+            success: true, 
+            message: 'Compte créé avec succès',
+            data: cv_cand
+          });
     
-    // add candidate
-    dict = req.body
-    const candidate = new Candidate({
-        mail: dict.email,
-        mdp: dict.mdp,
-        num_tel: dict.num_tel,
-    }) 
-    await candidate.save()
 
-    // add cv 
-    const cv_cand = new cv()
-    cv_cand.id_cand = candidate._id
-    cv_cand.github = dict.github
-
-    await cv_cand.save()
-
-
-     
-    // add competences
-    for(var k=0; k< dict["competences"]["competences"].length; k++ ){
-        const comp = new competence()
-        comp.id_cv = cv_cand._id
-        comp.detail= dict.competences["competences"][k]["comp"]
-        await comp.save()
+    } catch (error) {
+        return res.json({ 
+            success: false, 
+            message: 'Error:'+error.message,
+          });
     }
-    console.log("competences")
-
     
 
-    // add experience
-    for(var j=0; j< dict["experiences"]["experiences"].length ; j++ ){
-
-        exp_dict = dict.experiences["experiences"][j]
-        const exp = new experience()
-    
-        exp.id_cv = cv_cand._id
-        exp.duree = exp_dict["duree"]
-        exp.detail= exp_dict["detail"]
-        await exp.save()
-    }
-    console.log("experiences")
-
-    
-    // send result
-
-    res.send(cv_cand)
-    console.log("cv Ajouté")
 }
+ 
